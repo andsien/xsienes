f51a19385082381421ab6e1ac8691518a1a99345
Dennis Keith Gaso <andsien@gmail.com>
2018-03-26 17:10:50 +0800

    Added fancy box for website portfolio

--- index.php
+++ index.php
@@ -2,16 +2,25 @@
 <html>
 <head lang="en">
     <meta charset="UTF-8">
-    <title>Joseph Andrew Sienes Portfolio</title>
+    <title>Joseph Andrew Sienes</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
+    <meta name="description" content="I’m an experienced Scrum Master with a demonstrated history of working in the internet industry. Skilled in PHP, WordPress, User Interface Design, XHTML, and jQuery. Strong engineering professional with a Information Management focused in Management Information Systems, General from Silliman University." />
+    <meta name="author" content="Joseph Andrew Sienes" />
+
+    <meta property="og:url" content="http://xsienes.com" />
+    <meta property="og:type" content="article" />
+    <meta property="og:title" content="Joseph Andrew Sienes" />
+    <meta property="og:description" content="I’m an experienced Scrum Master with a demonstrated history of working in the internet industry. Skilled in PHP, WordPress, User Interface Design, XHTML, and jQuery. Strong engineering professional with a Information Management focused in Management Information Systems, General from Silliman University." />
+    <meta property="og:image" content="http://xsienes.com/images/xdface.jpg" />
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.5/jquery.fancybox.min.css" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" integrity="sha384-3AB7yXWz4OeoZcPbieVW64vVXEwADiYyAEhwilzWsLw+9FgqpyjjStpPnpBO8o8S" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css" crossorigin="anonymous">
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.5/jquery.fancybox.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script src="js/script.js" crossorigin="anonymous"></script>
@@ -20,7 +29,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-3 xnavi">
-                <img src="images/xface.jpg" class="xface rounded-circle" alt="Joseph Andrew Sienes">
+                <img src="images/xdface.jpg" class="xface rounded-circle" alt="Joseph Andrew Sienes">
                 <div class="xnavi-i-container">
                     <div class="xicon-holder text-center rounded-circle xdgray">
                         <a href="#" data-toggle="modal" data-target="#xcontact-modal"><i class="fas fa-comment-alt"></i></a>
@@ -47,17 +56,28 @@
                 <div class="xwork">
                     <div class="row">
                         <div class="col-lg-4 col-md-4 col-sm-12 xwork-col">
-                            <div class="text-center"><img src="images/wweb.jpg" class="rounded-circle" alt="web developer"></div>
-                            <div>Started web development back in College around 12yrs ago. Uses PHP Laravel framework, mysql on web applications and vuejs on frontend. </div>
+                            <div class="text-center">
+                                <a data-fancybox="xgallery" href="images/coopbank.png" data-thumb="images/coopbank.png">
+                                    <img src="images/wweb.jpg" class="rounded-circle" alt="web developer">
+                                </a>
+                            </div>
+                            <div>Started web development back in College around 12 years ago. Uses PHP Laravel framework, mysql on web applications and vuejs on frontend. </div>
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-sm-12  xwork-col">
+                            <div class="text-center">
+                                <a data-fancybox data-type="iframe" data-src="http://xsienes.com/file/Joseph%20Andrew%20Lopez%20Sienes-ScrumAlliance_CSM_Certificate.pdf">
+                                    <img src="images/wcsm.jpg" class="rounded-circle" alt="certified scrum master"/>
+                                </a>
+                            </div>
+                            <div>I have 3 years experience in scrum methodology. I facilitate development teams that uses agile development and mananges the process on how information is exchanged</div>
                         </div>
                         <div class="col-lg-4 col-md-4 col-sm-12  xwork-col">
                             <div class="text-center"><img src="images/wmobile.jpg" class="rounded-circle" alt="mobile developer"></div>
-                            <div>3 years experience in iOS development. Code using native language objective-c and swift. I do also have 1year experience working on Xamarin.</div>
+                            <div>3 years experience in iOS development. Code using native language objective-c and swift. I do also have 1 year experience working on Xamarin.</div>
                         </div>
-                        <div class="col-lg-4 col-md-4 col-sm-12  xwork-col">
-                            <div class="text-center"><img src="images/wcsm.jpg" class="rounded-circle" alt="certified scrum master"></div>
-                            <div>I have 5 years experience in scrum methodology. I facilitate development teams that uses agile development and mananges the process on how information is exchanged</div>
-                        </div>
+                    </div>
+                    <div class="hidden">
+                        <a data-fancybox="xgallery" href="images/pinoyfootball.png" data-thumb="images/coopbank.png"></a>
                     </div>
                 </div>
             </div>
