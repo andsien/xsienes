5d4f787e4114eba09bee160b617d42a118fe1d05
Dennis Keith Gaso <andsien@gmail.com>
2018-04-05 23:33:22 +0800

    added ads script for smooth element animation

--- index.php
+++ index.php
@@ -14,68 +14,67 @@
     <meta property="og:description" content="I’m an experienced Scrum Master with a demonstrated history of working in the internet industry. Skilled in PHP, WordPress, User Interface Design, XHTML, and jQuery. Strong engineering professional with a Information Management focused in Management Information Systems, General from Silliman University." />
     <meta property="og:image" content="http://xsienes.com/images/xdface.jpg" />
 
+    <link rel="stylesheet" href='//fonts.googleapis.com/css?family=Lato:300,400,600' type="text/css" media="all" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.5/jquery.fancybox.min.css" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" integrity="sha384-3AB7yXWz4OeoZcPbieVW64vVXEwADiYyAEhwilzWsLw+9FgqpyjjStpPnpBO8o8S" crossorigin="anonymous">
+    <link rel="stylesheet" href="lib/aos/aos.css" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css" crossorigin="anonymous">
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.5/jquery.fancybox.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script src="js/script.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <div class="container-fluid">
         <div class="row">
             <div class="col-3 xnavi">
                 <a data-fancybox href="images/andrew.jpg" data-thumb="images/andrew.jpg">
-                    <img src="images/xdface.jpg" class="xface rounded-circle" alt="Joseph Andrew Sienes">
+                    <img data-aos="flip-left" src="images/xdface.jpg" class="xface rounded-circle" alt="Joseph Andrew Sienes">
                 </a>
                 <div class="xnavi-i-container">
-                    <div class="xicon-holder text-center rounded-circle xdgray">
+                    <div data-aos="fade-right" class="xicon-holder text-center rounded-circle xdgray">
                         <a href="#" data-toggle="modal" data-target="#xcontact-modal"><i class="fas fa-comment-alt"></i></a>
                     </div>
-                    <div class="xicon-holder text-center rounded-circle xdblue">
+                    <div data-aos="fade-left" class="xicon-holder text-center rounded-circle xdblue">
                         <a href="https://www.linkedin.com/in/andsien/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
                     </div>
-                    <div class="xicon-holder text-center rounded-circle xlblack">
+                    <div data-aos="fade-right" class="xicon-holder text-center rounded-circle xlblack">
                         <a href="https://github.com/andsien" target="_blank"><i class="fab fa-github"></i></a>
                     </div>
-                    <div class="xicon-holder text-center rounded-circle xlblue">
+                    <div data-aos="fade-left" class="xicon-holder text-center rounded-circle xlblue">
                         <a href="https://www.facebook.com/andsien" target="_blank"><i class="fab fa-facebook-f"></i></a>
                     </div>
-                    <div class="xicon-holder text-center rounded-circle xbluGre">
+                    <div data-aos="fade-right" class="xicon-holder text-center rounded-circle xbluGre">
                         <a href="https://twitter.com/andsien" target="_blank"><i class="fab fa-twitter"></i></a>
                     </div>
                 </div>
             </div>
             <div class="col-9 xcontainer">
                 <div class="xabout">
-                    <h1>Hi, I’m Andrew!</h1>
-                    <p>I’m an experienced Scrum Master with a demonstrated history of working in the internet industry. Skilled in PHP, WordPress, User Interface Design, XHTML, and jQuery. Strong engineering professional with a Information Management focused in Management Information Systems, General from Silliman University.</p>
+                    <h1 data-aos="fade-down-right">Hi, I’m Andrew!</h1>
+                    <p data-aos="zoom-out-left">I’m an experienced Scrum Master with a demonstrated history of working in the internet industry. Skilled in PHP, WordPress, User Interface Design, XHTML, and jQuery. Strong engineering professional with a Information Management focused in Management Information Systems, General from Silliman University.</p>
                 </div>
                 <div class="xwork">
                     <div class="row">
                         <div class="col-lg-4 col-md-4 col-sm-12 xwork-col">
                             <div class="text-center">
                                 <a data-fancybox="xgallery" href="images/coopbank.jpg" data-thumb="images/coopbank.jpg">
-                                    <img src="images/wweb.jpg" class="rounded-circle" alt="web developer">
+                                    <img data-aos="zoom-in" src="images/wweb.jpg" class="rounded-circle" alt="web developer">
                                 </a>
                             </div>
-                            <div>Started web development back in College around 12 years ago. Uses PHP Laravel framework, mysql on web applications and vuejs on frontend. </div>
+                            <div data-aos="zoom-out-left">Started web development back in College around 12 years ago. Uses PHP Laravel framework, mysql on web applications and vuejs on frontend. </div>
                         </div>
                         <div class="col-lg-4 col-md-4 col-sm-12  xwork-col">
                             <div class="text-center">
                                 <a data-fancybox data-type="iframe" data-src="http://xsienes.com/file/Joseph%20Andrew%20Lopez%20Sienes-ScrumAlliance_CSM_Certificate.pdf">
-                                    <img src="images/wcsm.jpg" class="rounded-circle" alt="certified scrum master"/>
+                                    <img data-aos="zoom-in" src="images/wcsm.jpg" class="rounded-circle" alt="certified scrum master"/>
                                 </a>
                             </div>
-                            <div>I have 3 years experience in scrum methodology. I facilitate development teams that uses agile development and mananges the process on how information is exchanged</div>
+                            <div data-aos="zoom-out-right">I have 3 years experience in scrum methodology. I facilitate development teams that uses agile development and mananges the process on how information is exchanged</div>
                         </div>
                         <div class="col-lg-4 col-md-4 col-sm-12  xwork-col">
-                            <div class="text-center"><img src="images/wmobile.jpg" class="rounded-circle" alt="mobile developer"></div>
-                            <div>3 years experience in iOS development. Code using native language objective-c and swift. I do also have 1 year experience working on Xamarin.</div>
+                            <div class="text-center">
+                                <img data-aos="zoom-in" src="images/wmobile.jpg" class="rounded-circle" alt="mobile developer">
+                            </div>
+                            <div data-aos="zoom-out-left">3 years experience in iOS development. Code using native language objective-c and swift. I do also have 1 year experience working on Xamarin.</div>
                         </div>
                     </div>
                     <div class="hidden">
@@ -115,6 +114,13 @@
             </div>
         </div>
     </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.5/jquery.fancybox.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="lib/aos/aos.js" crossorigin="anonymous"></script>
+    <script src="js/script.js?new" crossorigin="anonymous"></script>
 
 </body>
 </html>
