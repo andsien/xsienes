8f6cd3c013bedd9f54b93249997ebbf814ddd54e
Joseph Andrew L. Sienes <andsien@Josephs-MBP.domain.name>
2018-08-02 14:30:41 +0800

    fix email php

--- php/mail.php
+++ php/mail.php
@@ -1,10 +1,11 @@
 <?php
 try {
+	$from = "contact@xsienes.com";
     $xcemail = $_POST['xcemail'];
     $xcmessage = $_POST['xcmessage'];
-
+    $headers = "From: $from";
     if($xcemail !="" && $xcmessage !="") {
-        mail($xcemail,"Xsienes Inquiry",$xcmessage);
+        mail($xcemail,"Xsienes Inquiry",$xcmessage, $headers, "-f " . $from);
         echo "Email Sent";
     }else{
         echo "Error Email not sent!";
